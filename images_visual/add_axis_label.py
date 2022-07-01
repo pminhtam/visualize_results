4a963cf40f87ea835aafb4bcd4b46e93fff6b778
MInh Tam <pminhtamnb@gmail.com>
2022-07-01 11:41:08 +0700

    fix title locate center

--- images_visual/add_axis_label.py
+++ images_visual/add_axis_label.py
@@ -3,11 +3,10 @@ import numpy as np
 from PIL import Image
 from PIL import Image, ImageDraw,ImageFont
 
-def add_x_label(image,label,distance,font_size=500):
-    xx_label = font_size*15/500
+def add_x_label_one_image(ypixels ,font_size=500,lb="Clean"):
+    xx_label = font_size*15/400
     # print(image)
     # height, width,c = image.shape
-    xpixels, ypixels, _ = image.shape
     # print(xpixels, ypixels)
     dpi = 10
     fig, ax = plt.subplots(figsize=(ypixels / dpi , xx_label),dpi=dpi)
@@ -15,18 +14,25 @@ def add_x_label(image,label,distance,font_size=500):
     plt.margins(0, 0)
     width, height = fig.get_size_inches()* fig.get_dpi()
     width, height = int(width), int(height)
-    text_label = ''
-    # text_label = 'Neigh2Neigh' + 10*' ' + ' Neigh2Neigh'
-    for lb in label:
-        distance_temp = distance - len(lb)
-        text_label += lb + distance_temp*' '
-    print(text_label)
-    plt.text(0.01, 0.6, text_label, fontsize=font_size,fontweight='bold',horizontalalignment='left',verticalalignment='top', transform=ax.transAxes)
-    # ax.imshow(image)
-
-    # ax.axis('off')
+    print(width,height)
+    plt.text(0.5, 0.5, lb, fontsize=font_size, horizontalalignment='center',
+                 verticalalignment='center', transform=ax.transAxes)
+    plt.show()
     fig.canvas.draw()  # draw the canvas, cache the renderer
 
     image = np.frombuffer(fig.canvas.tostring_rgb(), dtype='uint8').reshape(height, width, 3)
-    # print(image)
     return image
+
+from utils.merge_images import merge_images_row
+def add_x_label(image,label,font_size=10):
+
+    xpixels, ypixels, _ = image.shape
+    images_label_list = []
+    yy = (ypixels - 3*len(label)) / len(label)
+    for lb in label:
+        image = add_x_label_one_image(yy,font_size,lb)
+        images_label_list.append(Image.fromarray(image))
+    img_merge = merge_images_row(images_label_list, padding=3)
+    img_merge.show()
+    # exit()
+    return img_merge
